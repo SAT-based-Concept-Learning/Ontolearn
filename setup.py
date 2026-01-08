ae7d92107bfa87e47c8008423e901f8f32d34897
mfunk <mfunk@informatik.uni-leipzig.de>
2026-01-08 13:20:29 +0100

    Change python-sat dependency to allow newer versions

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ _deps = [
     "uvicorn>=0.29.0",
     "openai>=1.86.0",
     "lxml==5.3.0",
-    "python-sat==0.1.7.dev23",
+    "python-sat>=0.1.7.dev23",
     "asciitree==0.3.3"]
 
 deps = {b: a for a, b in (re.findall(r"^(([^!=<>~ ]+)(?:[!=<>~ ].*)?$)", x)[0] for x in _deps)}
